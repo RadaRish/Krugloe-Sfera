02904cbcfeea6d5c535e1b510d0ed632536021f8
RadaRish <107124341+RadaRish@users.noreply.github.com>
2025-05-01 16:56:55 +0300

    Upd3

--- data.js
+++ data.js
@@ -1,8 +1,8 @@
 var APP_DATA = {
   "scenes": [
     {
-      "id": "0-1",
-      "name": "1",
+      "id": "0--",
+      "name": "Вид сверху",
       "levels": [
         {
           "tileSize": 256,
@@ -37,56 +37,14 @@ var APP_DATA = {
           "yaw": -0.24816259184598266,
           "pitch": 0.8576202581027434,
           "rotation": 0,
-          "target": "2-3"
+          "target": "1--"
         }
       ],
       "infoHotspots": []
     },
     {
-      "id": "1-2",
-      "name": "2",
-      "levels": [
-        {
-          "tileSize": 256,
-          "size": 256,
-          "fallbackOnly": true
-        },
-        {
-          "tileSize": 512,
-          "size": 512
-        },
-        {
-          "tileSize": 512,
-          "size": 1024
-        },
-        {
-          "tileSize": 512,
-          "size": 2048
-        },
-        {
-          "tileSize": 512,
-          "size": 4096
-        }
-      ],
-      "faceSize": 4096,
-      "initialViewParameters": {
-        "yaw": 0.7905806261764781,
-        "pitch": -0.018283512381906064,
-        "fov": 1.349242487947885
-      },
-      "linkHotspots": [
-        {
-          "yaw": 0.4919899155444565,
-          "pitch": 0.13133223144556005,
-          "rotation": 0,
-          "target": "2-3"
-        }
-      ],
-      "infoHotspots": []
-    },
-    {
-      "id": "2-3",
-      "name": "3",
+      "id": "1--",
+      "name": "У стола",
       "levels": [
         {
           "tileSize": 256,
@@ -121,13 +79,55 @@ var APP_DATA = {
           "yaw": 0.03533133496760854,
           "pitch": 0.22318877373334445,
           "rotation": 0,
-          "target": "1-2"
+          "target": "2---"
         },
         {
           "yaw": 2.440305329603193,
           "pitch": 0.17933464626102236,
           "rotation": 0,
-          "target": "0-1"
+          "target": "0--"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2---",
+      "name": "Ди-джей пульт",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "yaw": 0.7905806261764781,
+        "pitch": -0.018283512381906064,
+        "fov": 1.349242487947885
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.4919899155444565,
+          "pitch": 0.13133223144556005,
+          "rotation": 0,
+          "target": "1--"
         }
       ],
       "infoHotspots": []
